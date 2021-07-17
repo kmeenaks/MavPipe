46224171502af44ef2ebab2d7d5d74fb6c39a8d6
student <kmeenaks@gmail.com>
2021-07-17 16:23:50 +0000

    pipeline script2 added

---
+++ src/main/java/com/Simplilearn/mavtest/MavtestApplication.java
@@ -0,0 +1,13 @@
+package com.Simplilearn.mavtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavtestApplication.class, args);
+	}
+
+}
